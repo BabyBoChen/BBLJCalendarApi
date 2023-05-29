a12f86c836660f3ee11d8821fbf37d8fe05f6087
BabyBoChen <tc10131@hotmail.com>
2023-05-29 19:51:24 +0800

    2024

--- updateDb.js
+++ updateDb.js
@@ -2,7 +2,7 @@ const path = require("path");
 const updateCalendarApi = require("./services/updateCalendarService.js");
 
 function main() {
-    let jsonFilePath = path.join(process.cwd(), "data", "yyyy.json");
+    let jsonFilePath = path.join(process.cwd(), "data", "2024.json");
     updateCalendarApi(jsonFilePath);
 }
 
