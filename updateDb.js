32d2bbae57676a3a96e2bc94ad983ec3135bbeb5
boychen <boychen@top-team.com.tw>
2024-08-15 12:25:57 +0800

    2025

--- updateDb.js
+++ updateDb.js
@@ -2,7 +2,7 @@ const path = require("path");
 const updateCalendarApi = require("./services/updateCalendarService.js");
 
 function main() {
-    let jsonFilePath = path.join(process.cwd(), "data", "2024.json");
+    let jsonFilePath = path.join(process.cwd(), "data", "2025.json");
     updateCalendarApi(jsonFilePath);
 }
 
